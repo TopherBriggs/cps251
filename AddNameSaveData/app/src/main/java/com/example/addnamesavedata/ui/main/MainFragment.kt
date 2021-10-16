5e8743d3844461a2f0e483fd99b7f96cf7c0ad95
Topher Briggs <christophertbriggs@gmail.com>
2021-10-16 14:11:17 -0400

    implmented Viewmodel to project

--- AddNameSaveData/app/src/main/java/com/example/addnamesavedata/ui/main/MainFragment.kt
+++ AddNameSaveData/app/src/main/java/com/example/addnamesavedata/ui/main/MainFragment.kt
@@ -7,8 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.addnamesavedata.R
+import com.example.addnamesavedata.databinding.MainFragmentBinding
+
+
 
 class MainFragment : Fragment() {
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = MainFragment()
@@ -20,13 +26,25 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.message.text = viewModel.getName()
+
+        binding.button.setOnClickListener {
+            viewModel.addName(binding.nameInput.text.toString())
+            binding.message.text = viewModel.getName()
+        }
+
     }
 
 }
--- AddNameSaveData/app/src/main/java/com/example/addnamesavedata/ui/main/MainViewModel.kt
+++ AddNameSaveData/app/src/main/java/com/example/addnamesavedata/ui/main/MainViewModel.kt
@@ -3,5 +3,19 @@ package com.example.addnamesavedata.ui.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private var names = "";
+
+    fun addName(name : String) {
+        if(name.isNotBlank()) {
+            this.names += "\n" + name
+        }
+    }
+
+    fun getName() : String {
+        if(names.isBlank()) {
+            return "No names to display"
+        }
+        return names;
+    }
+
 }
