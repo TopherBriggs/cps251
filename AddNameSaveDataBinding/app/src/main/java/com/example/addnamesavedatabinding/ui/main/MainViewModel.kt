449117d6c2aec269c4ea63a82e52c4756b6bcc33
Topher Briggs <christophertbriggs@gmail.com>
2021-10-23 19:07:36 -0400

    Updated xml file

---
+++ AddNameSaveDataBinding/app/src/main/java/com/example/addnamesavedatabinding/ui/main/MainViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.addnamesavedatabinding.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private var names = "";
+
+    fun addName(name : String) {
+        if(name.isNotBlank()) {
+            this.names += "\n" + name
+        }
+    }
+
+    fun getName() : String {
+        if(names.isBlank()) {
+            return "No names to display"
+        }
+        return names;
+    }
+
+}
