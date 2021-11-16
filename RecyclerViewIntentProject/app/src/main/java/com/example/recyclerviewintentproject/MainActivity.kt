6b123de018fde9b64cb3a61a442ef666ac4f6af3
Topher Briggs <christophertbriggs@gmail.com>
2021-11-15 19:36:57 -0500

    Intial Project

---
+++ RecyclerViewIntentProject/app/src/main/java/com/example/recyclerviewintentproject/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.recyclerviewintentproject
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
+import androidx.lifecycle.ViewModelProvider
+
+
+import com.example.recyclerviewintentproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        layoutManager = LinearLayoutManager(this)
+        binding.contentMain.recyclerView.layoutManager = layoutManager
+        adapter = RecyclerAdapter(viewModel.getTitles(), viewModel.getDetails(), viewModel.getImages())
+        binding.contentMain.recyclerView.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
---
+++ RecyclerViewIntentProject/app/src/main/java/com/example/recyclerviewintentproject/MainActivity2.kt
@@ -0,0 +1,5 @@
+package com.example.recyclerviewintentproject
+
+class MainActivity2 {
+    
+}
